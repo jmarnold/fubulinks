72cffd2dbb73ad0a461e03c25d21a9873aa35c73
jmarnold <rncodepoet@gmail.com>
2011-08-12 10:09:32 -0500

    basic fubu bootstrapping

---
+++ src/FubuLinks/Configuration/FubuLinksRegistry.cs
@@ -0,0 +1,18 @@
+﻿using FubuMVC.Core;
+
+namespace FubuLinks.Configuration
+{
+    public class FubuLinksRegistry : FubuRegistry
+    {
+        public FubuLinksRegistry()
+        {
+            IncludeDiagnostics(true);
+
+            Applies
+                .ToThisAssembly();
+
+            Views
+                .TryToAttachWithDefaultConventions();
+        }
+    }
+}
---
+++ src/FubuLinks/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Routing;
+using FubuLinks.Configuration;
+using FubuMVC.Core;
+using FubuMVC.StructureMap;
+
+namespace FubuLinks
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            FubuApplication
+                .For<FubuLinksRegistry>()
+                .StructureMapObjectFactory(x => { })
+                .Bootstrap(RouteTable.Routes);
+        }
+    }
+}
